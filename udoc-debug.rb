c5f490c0995227878777ea7200af89b59cc77e0b
bill-auger <mr.j.spam.me@gmail.com>
2016-10-12 13:11:06 -0400

    correlate source<->doc headers

---
+++ udoc-debug.rb
@@ -0,0 +1,17 @@
+
+DEBUG = false
+
+def DBG dbg
+  print "#{dbg}\n" if DEBUG
+end
+
+def dDEBUG_TRACE_N_HEADERS data_hash
+  DBG "#{data_hash[HEADERS_KEY].size} header files found in '#{data_hash[DIR_KEY]}'"
+#   DBG "\tsource_headers=#{data_hash[HEADERS_KEY]}"
+end
+
+def dDEBUG_TRACE_CTAG header , symbol , role , line_n , filename , prototype , is_override , is_virtual
+  DBG "\n[ENTITY]: #{header}::#{symbol}"                                                   +
+      "\n\tfilename=#{filename}\n\tsymbol=#{symbol}\n\trole=#{role}\n\tline_n=#{line_n}"   +
+      "\n\tprototype=#{prototype}\n\tis_virtual=#{is_virtual}\n\tis_override=#{is_override}"
+end
