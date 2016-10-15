e46e82e1b11ae0b1beb27eefe182872252233fd4
bill-auger <mr.j.spam.me@gmail.com>
2016-10-15 09:28:53 -0400

    write doc headers

--- udoc-debug.rb
+++ udoc-debug.rb
@@ -10,8 +10,12 @@ def dDEBUG_TRACE_N_HEADERS data_hash
 #   DBG "\tsource_headers=#{data_hash[HEADERS_KEY]}"
 end
 
-def dDEBUG_TRACE_CTAG header , symbol , role , line_n , filename , prototype , is_override , is_virtual
-  DBG "\n[ENTITY]: #{header}::#{symbol}"                                                   +
-      "\n\tfilename=#{filename}\n\tsymbol=#{symbol}\n\trole=#{role}\n\tline_n=#{line_n}"   +
-      "\n\tprototype=#{prototype}\n\tis_virtual=#{is_virtual}\n\tis_override=#{is_override}"
+def dDEBUG_TRACE_PARSE_CTAG header , symbol , role , line_n , filename , prototype , is_override , is_virtual
+  DBG "\n[ENTITY]: #{header}::#{symbol}"                                                     +
+      "\n\tfilename=#{filename}\n\tsymbol=#{symbol}\n\trole=#{role}\n\tline_n=#{line_n}"     +
+      "\n\tprototype=#{prototype}\n\tis_virtual=#{is_virtual}\n\tis_override=#{is_override}\n"
+end
+
+def dDEBUG_TRACE_LOAD_CTAG ctag
+  DBG "ctag=" + (((ctag.select {|ea| ea.is_a? String }).to_s.gsub ",",",\n\t").gsub "=>"," => ") + "\n"
 end
