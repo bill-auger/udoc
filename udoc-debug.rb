22891c430312db11033d4d78a60d1ad6e33c7240
bill-auger <mr.j.spam.me@gmail.com>
2016-10-16 10:27:18 -0400

    refactor

--- udoc-debug.rb
+++ udoc-debug.rb
@@ -5,14 +5,14 @@ def DBG dbg
   print "#{dbg}\n" if DEBUG
 end
 
-def dDEBUG_TRACE_N_HEADERS data_hash
-  DBG "#{data_hash[HEADERS_KEY].size} header files found in '#{data_hash[DIR_KEY]}'"
-#   DBG "\tsource_headers=#{data_hash[HEADERS_KEY]}"
+def dDEBUG_TRACE_N_HEADERS headers , directory
+  DBG "#{headers.size} header files found in '#{directory}'"
+#   DBG "\theaders=#{headers}"
 end
 
-def dDEBUG_TRACE_PARSE_CTAG header , symbol , role , line_n , filename , prototype , is_override , is_virtual
-  DBG "\n[ENTITY]: #{header}::#{symbol}"                                                     +
-      "\n\tfilename=#{filename}\n\tsymbol=#{symbol}\n\trole=#{role}\n\tline_n=#{line_n}"     +
+def dDEBUG_TRACE_PARSE_CTAG filename , identifier , line_n , kind , prototype , is_virtual , is_override
+  DBG "\n[ENTITY]: #{filename}::#{identifier}"                                                   +
+      "\n\tfilename=#{filename}\n\tidentifier=#{identifier}\n\tkind=#{kind}\n\tline_n=#{line_n}" +
       "\n\tprototype=#{prototype}\n\tis_virtual=#{is_virtual}\n\tis_override=#{is_override}\n"
 end
 
